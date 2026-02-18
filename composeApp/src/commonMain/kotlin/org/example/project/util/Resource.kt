51a8414654b931664e1d9d99287ae038aa1ce84c
kay <kayshiro@live.no>
2026-02-18 18:10:53 +0100

    Adding Icon/Image

---
+++ composeApp/src/commonMain/kotlin/org/example/project/util/Resource.kt
@@ -0,0 +1,39 @@
+package org.example.project.util
+
+import to_do_km.composeapp.generated.resources.Res
+import to_do_km.composeapp.generated.resources.arrow_back
+import to_do_km.composeapp.generated.resources.blank_box
+import to_do_km.composeapp.generated.resources.check_box
+import to_do_km.composeapp.generated.resources.close
+import to_do_km.composeapp.generated.resources.delete
+import to_do_km.composeapp.generated.resources.edit
+import to_do_km.composeapp.generated.resources.hamburger_menu
+import to_do_km.composeapp.generated.resources.painting_dark
+import to_do_km.composeapp.generated.resources.painting_light
+import to_do_km.composeapp.generated.resources.pen_and_paper_light
+import to_do_km.composeapp.generated.resources.search
+import to_do_km.composeapp.generated.resources.sort
+import to_do_km.composeapp.generated.resources.warning_dark
+import to_do_km.composeapp.generated.resources.warning_light
+
+object Resource {
+    object Icon {
+        val BACK_ARROW = Res.drawable.arrow_back
+        val BLANK_BOX = Res.drawable.blank_box
+        val CHECK_BOX = Res.drawable.check_box
+        val CLOSE = Res.drawable.close
+        val DELETE = Res.drawable.delete
+        val EDIT = Res.drawable.edit
+        val HAMBURGER_MENU = Res.drawable.hamburger_menu
+        val SEARCH = Res.drawable.search
+        val SORT = Res.drawable.sort
+    }
+
+    object Image {
+        val PAINTING_LIGHT = Res.drawable.painting_light
+        val PAINTING_DARK = Res.drawable.painting_dark
+        val PEN_PAPER_LIGHT = Res.drawable.pen_and_paper_light
+        val WARNING_LIGHT = Res.drawable.warning_light
+        val WARNING_DARK = Res.drawable.warning_dark
+    }
+}
